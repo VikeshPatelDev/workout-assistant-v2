85695a4544368297ce70e8bbc320cf115fc11234
VikeshPatelDev <32527657+VikeshPatelDev@users.noreply.github.com>
2026-01-18 13:41:58 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/workout-assistant-v2/sw.js', { scope: '/workout-assistant-v2/' })})}
